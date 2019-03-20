2d9404e1092694aab58febec4c6ddd22207865d6
Johannes Deivard <jdeivard@outlook.com>
2019-03-21 00:45:41 +0100

    Modified for sub cipher

--- breaking_the_cipher/breaking_the_cipher.fs
+++ breaking_the_cipher/breaking_the_cipher.fs
@@ -1,12 +1,19 @@
 module breaking_the_cipher
 open caesarDecrypter
+open substitutionDecrypter
+
 
 
 [<EntryPoint>]
 let main argv =
-    let path = "../../../ciphers/TEXT_1.txt"
-    let text = System.IO.File.ReadAllText path
-    
-    let decryptedText = caesarDecrypter.decrypt text
-    
+    let path1 = "../../../ciphers/TEXT_1.txt"
+    let text1 = System.IO.File.ReadAllText path1
+    let path2 = "../../../ciphers/TEXT_2.txt"
+    let text2 = System.IO.File.ReadAllText path2
+
+
+    //let decryptedText = caesarDecrypter.decrypt text
+    let decryptedSubCipher = substitutionDecrypter.decrypt text2
+    printf "Final text: \n--------------------\n%s" decryptedSubCipher 
+
     0 // return an integer exit code
