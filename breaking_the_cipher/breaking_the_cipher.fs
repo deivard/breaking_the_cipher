922ec05cdc5c053713c077349858e119af646782
Johannes Deivard <jdeivard@outlook.com>
2019-03-24 16:05:02 +0100

    small fix

--- breaking_the_cipher/breaking_the_cipher.fs
+++ breaking_the_cipher/breaking_the_cipher.fs
@@ -11,30 +11,23 @@ let main argv =
     let text2 = System.IO.File.ReadAllText path2
 
     let rec programLoop () =
-        printf "Breaking the cipher.\n
-                Press 1 for part 1 (Caesar cipher)\n
-                Press 2 for part 2 (Substition cipher)\n
-                Press 3 for part 3 (Vignere cipher)\n
+        printf "\nBreaking the cipher.\n\
+                Press 1 for part 1 (Caesar cipher)\n\
+                Press 2 for part 2 (Substition cipher)\n\
+                Press 3 for part 3 (Vignere cipher)\n\
                 Press any other key to quit.\n"
-        let c = Console.ReadKey()
-        match c.KeyInfo with
-        | '1' -> caesarDecrypter.decrypt text1 |> printf "Final text: \n--------------------\n%s"; programLoop
-        | '2' -> substitutionDecrypter.decrypt text2 |> printf "Final text: \n--------------------\n%s"; programLoop
+        let c = System.Console.ReadKey()
+        printf "\n"
+        match c.KeyChar with
+        | '1' -> caesarDecrypter.decrypt text1 |> printf "Final text: \n--------------------\n%s"; programLoop ()
+        | '2' -> substitutionDecrypter.decrypt text2 |> printf "Final text: \n--------------------\n%s"; programLoop ()
         | '3' -> 
             vignereCipher.vignereCrypt |> ignore
             System.Console.ReadKey() |> ignore
             vignereCipher.vignereDecrypt |> ignore
-            programLoop
+            programLoop ()
         | _ -> 0
 
-    let decryptedText = caesarDecrypter.decrypt text1
-    let decryptedSubCipher = substitutionDecrypter.decrypt text2
-    printf "Final text: \n--------------------\n%s" decryptedSubCipher 
-    printf "Final text: \n--------------------\n%s" decryptedText 
-
-
-    vignereCipher.vignereCrypt |> ignore
-    System.Console.ReadKey() |> ignore
-    vignereCipher.vignereDecrypt |> ignore
+    programLoop()
 
     0 // return an integer exit code
